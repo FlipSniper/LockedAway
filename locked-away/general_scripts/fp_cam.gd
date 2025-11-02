96b05adcf42210273a76cba16094e80b417216bc
FlipSniper <m-navaneeth21@st-bernards.slough.sch.uk>
2025-11-02 11:28:16 +0000

    reset camera script for cutscenes

--- locked-away/general_scripts/fp_cam.gd
+++ locked-away/general_scripts/fp_cam.gd
@@ -3,6 +3,8 @@ extends Node3D
 var sensitivity = 0.2
 var flashlight
 
+@onready var player = get_tree().current_scene.get_node("player")
+
 func _ready() -> void:
 	flashlight = get_tree().current_scene.get_node("flashlight2")
 
@@ -10,3 +12,12 @@ func _process(delta: float) -> void:
 	if player.controls_enabled:
 		if Input.is_action_just_pressed("flashlight"):
 			flashlight.visible = !flashlight.visible
+
+func _input(event: InputEvent) -> void:
+	if not player.controls_enabled:
+		player.rotation_degrees.x = 0
+		player.rotation_degrees.y = 0
+		player.rotation_degrees.z = 0
+		rotation_degrees.x = 0
+		rotation_degrees.y =0
+		rotation_degrees.z = 0
