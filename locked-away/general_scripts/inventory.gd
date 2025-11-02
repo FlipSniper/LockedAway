e62aae27a9502d8ad755f14503e5cb40d757fc79
FlipSniper <m-navaneeth21@st-bernards.slough.sch.uk>
2025-11-02 16:28:19 +0000

    limited amount of inventory space + unlock more inventory space

--- locked-away/general_scripts/inventory.gd
+++ locked-away/general_scripts/inventory.gd
@@ -1,1 +1,21 @@
 extends Node
+
+signal inventory_updated()
+signal slot_unlocked(new_total_slots)
+
+var mouse_lock = true
+@onready var max_slots: int = 8
+@onready var unlocked_slots: int = 1
+
+var slots: Array = []
+
+func _ready():
+	slots.resize(max_slots)
+	for i in range(max_slots):
+		slots[i] = null
+
+func unlock_slot() -> void:
+	if unlocked_slots < max_slots:
+		unlocked_slots += 1
+		emit_signal("slot_unlocked", unlocked_slots)
+		print("Unlocked slot ", unlocked_slots, "/", max_slots)
