13246e4a3ed6aabe27b3718f81f8b16d92f954e3
FlipSniper <m-navaneeth21@st-bernards.slough.sch.uk>
2025-11-02 16:48:06 +0000

    UI + Add item to inventory

--- locked-away/general_scripts/inventory.gd
+++ locked-away/general_scripts/inventory.gd
@@ -19,3 +19,30 @@ func unlock_slot() -> void:
 		unlocked_slots += 1
 		emit_signal("slot_unlocked", unlocked_slots)
 		print("Unlocked slot ", unlocked_slots, "/", max_slots)
+
+func add_item(item_name: String) -> bool:
+	for i in range(unlocked_slots):
+		if slots[i] == null:
+			slots[i] = item_name
+			emit_signal("inventory_updated")
+			print("Added ", item_name, " to slot ", i + 1)
+			return true
+	print("No free slots! Item not added: ", item_name)
+	return false
+
+func remove_item(slot_index: int) -> void:
+	if slot_index >= 0 and slot_index < unlocked_slots:
+		if slots[slot_index] != null:
+			print("Removed ", slots[slot_index], " from slot ", slot_index + 1)
+			slots[slot_index] = null
+			emit_signal("inventory_updated")
+		else:
+			print("Slot is already empty.")
+	else:
+		print("Invalid slot index")
+
+func find_item(item_name: String) -> int:
+	for i in range(unlocked_slots):
+		if slots[i] == item_name:
+			return i
+	return -1
